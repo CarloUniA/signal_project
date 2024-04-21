fa596842a5b9182be79fb2122a8c35e9431b4f05
Carlo <carlo.surmann01@gmail.com>
2024-04-21 14:00:55 +0200

    TcpOutputStrategy.java aligned to Google Javadoc guide

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -5,13 +5,27 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
-
+/**
+ * Implements the OutputStrategy interface to send patient data over a TCP connection.
+ * This strategy sets up a TCP server on a specified port and sends data to a connected client.
+ *
+ * It handles the creation of a server socket, awaits client connections, and then sends data
+ * formatted as a string to the client.
+ */
 public class TcpOutputStrategy implements OutputStrategy {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
 
+    /**
+     * Initializes a TCP server on the given port and prepares to accept client connections.
+     * Once a client is connected, initializes a PrintWriter to send data over the socket.
+     *
+     * @param port the TCP port where the server will listen for incoming client connections.
+     * @throws IOException if an error occurs when creating the server socket or when accepting a client connection.
+     *                     This exception is caught and logged, and does not terminate the method execution.
+     */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -32,6 +46,15 @@ public class TcpOutputStrategy implements OutputStrategy {
         }
     }
 
+    /**
+     * Sends the provided data to the connected client through the TCP connection.
+     * The data is sent as a formatted string using the format: "patientId,timestamp,label,data".
+     *
+     * @param patientId the unique identifier of the patient whose data is being sent.
+     * @param timestamp the time at which the data was recorded, in milliseconds since the epoch.
+     * @param label a descriptor of the type of data being sent (e.g., "heart rate").
+     * @param data the actual data string to be sent to the client.
+     */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
